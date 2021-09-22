e9092b6d8abb8beade881db0540b07da08f31d6b
cxd <15068808239@163.com>
2021-09-22 11:43:28 +0800

    feat: (ios) ios项目初始化

---
+++ ios/plugin/plugin/blue/BlueViewController.h
@@ -0,0 +1,37 @@
+//
+//  ViewController.h
+//  bluetooth
+//
+//  Created by cxd on 2021/9/16.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreBluetooth/CoreBluetooth.h>
+#import <WebKit/WebKit.h>
+
+@interface BlueViewController : UIViewController<CBCentralManagerDelegate,CBPeripheralDelegate,WKScriptMessageHandler>{
+}
+@property(nonatomic,strong)CBCentralManager *myCentralManager;
+@property(nonatomic,strong)CBPeripheral *peripheral;
+@property(nonatomic,strong)CBCharacteristic *characteristic;
+@property WKWebView *myWebView;
+
+- (void) initWebView;
+- (void) initBlue;
+- (void) scan;
+- (void) stopScan;
+- (void) connectPeripheral:(CBPeripheral*)peripheral;
+
+- (void) write;
+
+- (BOOL)isBluetoothAvailabel;
+
+//十六进制字符串转NSData
+- (NSData *)convertHexStrToData:(NSString *)str;
+
+//NSData转十六进制字符串
+- (NSString *)convertDataToHexStr:(NSData *)data;
+
+
+@end
+
