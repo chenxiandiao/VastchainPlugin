989cf946c90033979c2e48d22a76246d922414e3
chenxiandiao <15068808239@163.com>
2022-03-02 21:36:56 +0800

    feat: (ios) jsbridge添加

---
+++ ios/VastchainPlugin/VastchainPlugin/Classes/IJsBridge.h
@@ -0,0 +1,22 @@
+//
+//  IJsBridge.h
+//  Pods
+//
+//  Created by cxd on 2022/2/23.
+//
+
+#ifndef IJsBridge_h
+#define IJsBridge_h
+
+
+#endif /* IJsBridge_h */
+
+#import <Foundation/Foundation.h>
+#import <WebKit/WebKit.h>
+
+@interface IJsBridge : NSObject {
+    WKWebView *myWebView;
+}
+- (id) initWithWebView: (WKWebView*) webview;
+- (void) navigateBack;
+@end
