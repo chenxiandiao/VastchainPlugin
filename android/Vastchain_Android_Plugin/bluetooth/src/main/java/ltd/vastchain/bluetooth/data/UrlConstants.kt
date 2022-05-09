e5eaf9f4756735c73cfde22fd0ab9b34e95c37a3
chenxiandiao@vastchain.ltd <Cxd316213!>
2022-05-09 13:16:29 +0800

    feat: (Android) 二次确认url添加

--- android/Vastchain_Android_Plugin/bluetooth/src/main/java/ltd/vastchain/bluetooth/data/UrlConstants.kt
+++ android/Vastchain_Android_Plugin/bluetooth/src/main/java/ltd/vastchain/bluetooth/data/UrlConstants.kt
@@ -5,14 +5,22 @@ package ltd.vastchain.bluetooth.data
  */
 object UrlConstants {
     const val STOREHOUSE_HOME = "/softwareScan"
-    const val STOREHOUSE_SELL_OUT = "/storehouse/basic/exWarehousingSell"
-    const val STOREHOUSE_BACK = "/storehouse/basic/useBackWarehousing"
+//   领用回库
+    private const val STOREHOUSE_BACK = "/storehouse/basic/useBackWarehousing"
+    private const val STOREHOUSE_RETURN_GOODS = "/storehouse/basic/returnGoods"
+    private const val STOREHOUSE_MINBAO_Receive = "/storehouse/basic/componiesReceive"
+    private const val STOREHOUSE_MINBAO_USE = "/storehouse/basic/workspaceUse"
+    private const val STOREHOUSE_MINBAO_BACK = "'/storehouse/basic/returnTailing"
 
     private val BACK_CONFIRM_URL = mutableListOf<String>()
     init {
         BACK_CONFIRM_URL.apply {
 //            add(STOREHOUSE_SELL_OUT)
             add(STOREHOUSE_BACK)
+            add(STOREHOUSE_RETURN_GOODS)
+            add(STOREHOUSE_MINBAO_Receive)
+            add(STOREHOUSE_MINBAO_USE)
+            add(STOREHOUSE_MINBAO_BACK)
         }
     }
 
